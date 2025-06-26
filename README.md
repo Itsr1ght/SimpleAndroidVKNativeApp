9644d8bf63ed75ad88317827e9f9a35037b7c636
Bilal <bjlegend007@gmail.com>
2025-06-26 19:46:02 +0530

    added all the files

---
+++ README.md
@@ -0,0 +1,9 @@
+# SimpleAndroidNativeApp
+a simple android native opengl3 project in cpp without using java or kotlin
+
+
+## Build Instruction
+
+* install the gradle and android SDK and NDK
+* use ``` ./gradlew build ``` to build the project
+* use ``` ./gradlew installDebug ``` to install the debug for testing
---
+++ app/src/main/cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <android_native_app_glue.h>
+#include <android/log.h>
+#include <stdlib.h>
+
+#define LOG_TAG "RealNativeApp"
+#define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+
+void android_main(android_app* app) {
+}
