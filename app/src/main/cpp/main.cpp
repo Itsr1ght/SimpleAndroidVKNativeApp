30ef363734bc9c964e0765b28ce5c79a6f24d38e
Bilal <bjlegend007@gmail.com>
2025-06-26 22:43:57 +0530

    included vulkan into the project

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,8 +1,11 @@
+#define VK_USE_PLATFORM_ANDROID_KHR
+#include <vulkan/vulkan.h>
+
 #include <android_native_app_glue.h>
 #include <android/log.h>
 #include <stdlib.h>
 
-#define LOG_TAG "RealNativeApp"
+#define LOG_TAG "VKRealNativeApp"
 #define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
