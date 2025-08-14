be830b1d392d6646c08b6f8645f8ea3801a2fe1a
Bilal <bjlegend007@gmail.com>
2025-08-14 09:23:47 +0530

    added the volk and VulkanHeader to the CMakeLists.txt

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,14 +1,29 @@
-#define VK_USE_PLATFORM_ANDROID_KHR
-#include <vulkan/vulkan.h>
+#define VOLK_IMPLEMENTATION
+#include "volk.h"
 
 #include <android_native_app_glue.h>
 #include <android/log.h>
-#include <stdlib.h>
+#include <cstdlib>
 
 #define LOG_TAG "VKRealNativeApp"
 #define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 
+struct VulkanData{
+    VkInstance instance;
+};
+
+VulkanData data;
+
 void android_main(android_app* app) {
+
+    if (volkInitialize() == VK_FALSE){
+	LOG_ERROR("Cannot initialize volk");
+    }
+    
+    auto instance_create_info = VkInstanceCreateInfo {
+    };
+    vkCreateInstance(&instance_create_info, nullptr, &data.instance);
+
 }
