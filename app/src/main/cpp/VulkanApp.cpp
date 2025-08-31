40b2815892dce8780bc561f61d6c61378ada3d18
Bilal <bjlegend007@gmail.com>
2025-08-31 18:52:35 +0530

    enabled the debug layer

--- app/src/main/cpp/VulkanApp.cpp
+++ app/src/main/cpp/VulkanApp.cpp
@@ -20,6 +20,8 @@ void VulkanApp::create_instance() {
             "VK_KHR_android_surface"
     };
 
+    const char* layers[] = {"VK_LAYER_KHRONOS_validation"};
+
     auto application_info = VkApplicationInfo{
         .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
         .pApplicationName = "Hello Android",
@@ -32,8 +34,8 @@ void VulkanApp::create_instance() {
     auto instance_create_info = VkInstanceCreateInfo{
         .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
         .pApplicationInfo = &application_info,
-        .enabledLayerCount = 0,
-        .ppEnabledLayerNames = nullptr,
+        .enabledLayerCount = 1,
+        .ppEnabledLayerNames = layers,
         .enabledExtensionCount = 2,
         .ppEnabledExtensionNames = extensions,
     };
@@ -97,14 +99,28 @@ void VulkanApp::update() {
 }
 
 std::string VulkanApp::vkResultToString(VkResult result){
-    switch(result){
-        case VK_SUCCESS: return {"VK_SUCCESS"};
-        case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR: return {"VK_ERROR_NATIVE_WINDOW_IN_USE_KHR"};
-        case VK_ERROR_OUT_OF_DEVICE_MEMORY: return {"VK_ERROR_OUT_OF_DEVICE_MEMORY"};
-        case VK_ERROR_OUT_OF_HOST_MEMORY: return {"VK_ERROR_OUT_OF_HOST_MEMORY"};
-        case VK_ERROR_UNKNOWN: return {"VK_ERROR_UNKNOWN"};
-        case VK_ERROR_VALIDATION_FAILED_EXT: return {"VK_ERROR_VALIDATION_FAILED"};
-        default: return {"ERROR_UNKNOWN"};
+    switch (result) {
+        case VK_SUCCESS: return "VK_SUCCESS";
+        case VK_NOT_READY: return "VK_NOT_READY";
+        case VK_TIMEOUT: return "VK_TIMEOUT";
+        case VK_EVENT_SET: return "VK_EVENT_SET";
+        case VK_EVENT_RESET: return "VK_EVENT_RESET";
+        case VK_INCOMPLETE: return "VK_INCOMPLETE";
+        case VK_ERROR_OUT_OF_HOST_MEMORY: return "VK_ERROR_OUT_OF_HOST_MEMORY";
+        case VK_ERROR_OUT_OF_DEVICE_MEMORY: return "VK_ERROR_OUT_OF_DEVICE_MEMORY";
+        case VK_ERROR_INITIALIZATION_FAILED: return "VK_ERROR_INITIALIZATION_FAILED";
+        case VK_ERROR_DEVICE_LOST: return "VK_ERROR_DEVICE_LOST";
+        case VK_ERROR_MEMORY_MAP_FAILED: return "VK_ERROR_MEMORY_MAP_FAILED";
+        case VK_ERROR_LAYER_NOT_PRESENT: return "VK_ERROR_LAYER_NOT_PRESENT";
+        case VK_ERROR_EXTENSION_NOT_PRESENT: return "VK_ERROR_EXTENSION_NOT_PRESENT";
+        case VK_ERROR_FEATURE_NOT_PRESENT: return "VK_ERROR_FEATURE_NOT_PRESENT";
+        case VK_ERROR_INCOMPATIBLE_DRIVER: return "VK_ERROR_INCOMPATIBLE_DRIVER";
+        case VK_ERROR_TOO_MANY_OBJECTS: return "VK_ERROR_TOO_MANY_OBJECTS";
+        case VK_ERROR_FORMAT_NOT_SUPPORTED: return "VK_ERROR_FORMAT_NOT_SUPPORTED";
+        case VK_ERROR_FRAGMENTED_POOL: return "VK_ERROR_FRAGMENTED_POOL";
+        case VK_ERROR_UNKNOWN: return "VK_ERROR_UNKNOWN";
+            // add more from vk_result.h if needed
+        default: return "VK_RESULT_UNDEFINED";
     }
 }
 
